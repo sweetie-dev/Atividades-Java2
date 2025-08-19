9ed56aae9323d347891baa4c35fdb2caa511ed08
Emily Silva <3mysilva@gmail.com>
2025-08-19 10:23:34 -0300

    Arrays

---
+++ dia1.java
@@ -0,0 +1,10 @@
+package Arrays;
+public class dia1 {
+    public static void main(String[] args) {
+        String nomes[] = {"Lidia","Emily","Milena","Yasmin","Júlia"};
+        System.out.println(nomes[3]);
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Nome: "+  nomes[i] +"%n");
+        }
+    }
+}
