5c7382be9e1c977d6016ac87ffb5459155393266
Emily Silva <3mysilva@gmail.com>
2025-08-21 11:01:04 -0300

    Aula3

---
+++ Aula03.java
@@ -0,0 +1,15 @@
+//Aula 3
+package Arrays;
+public class Aula03{
+    public static void main(String[] args) {
+        // Criando um array de inteiros
+        int[] inicio = new int[5];
+        // Atribuindo valores ao array
+        inicio[0] = 10;
+        inicio[1] = 20;
+        inicio[2] = 30;
+        inicio[3] = 40; 
+        inicio[4] = 50;
+        System.out.println("o primeiro número é: "+inicio[0]);
+    }
+}
